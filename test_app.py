f38763f7a24769b7a3c9974286889634c62fe7b4
bakjeucar-boop <bakjeucar@gmail.com>
2026-08-19 22:47:57 +0900

    test: enlarge snapshot viewport to include board

--- test_app.py
+++ test_app.py
@@ -44,7 +44,7 @@ async def test_self_collision_causes_game_over() -> None:
 
 
 def test_initial_screen_snapshot(snap_compare) -> None:
-    assert snap_compare(SnakeApp(), terminal_size=(40, 12))
+    assert snap_compare(SnakeApp(), terminal_size=(80, 30))
 
 
 def test_after_move_screen_snapshot(snap_compare) -> None:
@@ -55,7 +55,7 @@ def test_after_move_screen_snapshot(snap_compare) -> None:
     assert snap_compare(
         SnakeApp(),
         run_before=run_before,
-        terminal_size=(40, 12),
+        terminal_size=(80, 30),
     )
 
 
